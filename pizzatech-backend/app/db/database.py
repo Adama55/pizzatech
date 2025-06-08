3b99696351e132c71ed585e9d09d402aacd23841
Adama55 <mafahfof55@gmail.com>
2025-06-08 09:55:21 +0200

    update database

--- pizzatech-backend/app/db/database.py
+++ pizzatech-backend/app/db/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from app.core.config import DATABASE_URL
+from dotenv import load_dotenv
+import os
 
-engine = create_engine(DATABASE_URL)
+load_dotenv()
+
+
+engine = create_engine(os.getenv("DATABASE_URL"))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 def get_db():
